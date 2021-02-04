8b680bee24da1e188274d33aa63867b09c2c8f28
ratnaraj297 <ratnarajwora@gmail.com>
2021-02-04 21:06:47 +0530

    Solved Example1

---
+++ Lab1/example/search_min.c
@@ -0,0 +1,14 @@
+int search(int arr[], int n){
+    int i;
+    int min = arr[0];
+
+    for(i =0; i<n; i++){
+        if(arr[i] < min){
+            min =  arr[i];
+        }
+    }
+
+    printf("Inside function, min = %d \n", min);
+    return min;
+
+}
